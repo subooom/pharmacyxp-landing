0be2022b64b3e229acaf1918d07f432539fd6e65
subooom <subhamkharel12@gmail.com>
2025-11-05 15:52:47 +0545

    added medicinexp-staging to host name

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,11 @@ const nextConfig: NextConfig = {
         hostname: "picsum.photos",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "medicinexp-staging.com",
+        pathname: "/**",
+      },
     ],
   },
   eslint: {
